fb12099133b5960ed5360d64636f76115e0e81d8
Bootler23 <s.klaeck@gmail.com>
2025-08-22 23:03:25 +0200

    customize -> sell_contidion & update BuyAmountFunktion

--- binance-java-api-8aa6fb51ada60ee31ec36be022a9e50a56fe439c/src/main/java/com/binance/api/tradingbot/TradingMain/LTC_EUR_Live.java
+++ binance-java-api-8aa6fb51ada60ee31ec36be022a9e50a56fe439c/src/main/java/com/binance/api/tradingbot/TradingMain/LTC_EUR_Live.java
@@ -103,7 +103,6 @@ public class LTC_EUR_Live {
                     ATHSQL.CheckForNewAllTimeHigh(currency, LivePrice);
 
                     BuyAmountFunktion.getBuyAmount(currency, EURO, client, LivePrice, false);
-
                     ATHSQL.updateLPP(currency);
 
                     if (count == 41 || FirstRound) {
